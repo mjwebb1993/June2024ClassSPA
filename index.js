e15d267851a61bb2973e23fb27e72037cbe0573c
Matt Thomas <matsinet@gmail.com>
2024-08-01 22:02:21 -0500

    Thurday 6.3 lesson

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import { header, nav, main, footer } from "./components";
+import * as store from "./store";
 
-function render() {
+function render(state = store.home) {
   document.querySelector("#root").innerHTML = `
-      ${header()}
-      ${nav()}
-      ${main()}
+      ${header(state)}
+      ${nav(store.nav)}
+      ${main(state)}
       ${footer()}
     `;
 }
 
 render();
+
 // add menu toggle to bars icon in nav bar
 // document.querySelector(".fa-bars").addEventListener("click", () => {
 //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
