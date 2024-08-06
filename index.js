5622d19229d0b3e714ba6f1213f1dd359404d97b
Michael Webb <mjwebb1993@yahoo.com>
2024-08-05 20:39:53 -0500

    6.3

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { header, nav, main, footer } from "./components";
 import * as store from "./store";
+import Navigo from "navigo";
+import { camelCase } from "lodash";
+
+const router = new Navigo("/");
 
 function render(state = store.home) {
   document.querySelector("#root").innerHTML = `
@@ -8,11 +12,32 @@ function render(state = store.home) {
       ${main(state)}
       ${footer()}
     `;
+  console.log("it's updating");
+  router.updatePageLinks();
+  console.log("it's updated");
 }
-
-render();
 
 // add menu toggle to bars icon in nav bar
 // document.querySelector(".fa-bars").addEventListener("click", () => {
 //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 // });
+
+router
+  .on({
+    "/": () => render(),
+    // Use object destructuring assignment to store the data and (query)params from the Navigo match parameter
+    // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+    // This reduces the number of checks that need to be performed
+    ":view": match => {
+      // Change the :view data element to camel case and remove any dashes (support for multi-word views)
+      const view = match?.data?.view ? camelCase(match.data.view) : "home";
+      // Determine if the view name key exists in the store object
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.viewNotFound);
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
