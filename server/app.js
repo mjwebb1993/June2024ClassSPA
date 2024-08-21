9651c6842a15ac7f2d5bde5384ef641cad8348ba
Matt Thomas <matsinet@gmail.com>
2024-08-20 20:25:11 -0500

    Finished class SPA and API

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import pizzas from "./routers/pizzas.js";
 
 // Initialize the Express application
 const app = express();
@@ -93,6 +94,8 @@ app.get("/weather/:city", (request, response) => {
     city
   });
 });
+
+app.use("/pizzas", pizzas);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
